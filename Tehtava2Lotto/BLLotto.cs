af920659e37a564cfde69035a13ead5ad29bb93f
Mikko Holopainen <h3828@student.jamk.fi>
2015-09-11 14:45:38 +0300

    Tehtävä 2 palautus

---
+++ Tehtava2Lotto/BLLotto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava2Lotto
+{
+    class Lotto
+    {
+        public int tyyppi; // 1: Lotto, 2: Viking Lotto, 3: Eurojackpot päänumerot, 4: Eurojackpot tähtinumerot
+        private int pieninNumero, suurinNumero, lkmNumero;
+        private Random arpoja;  // Lisätty jäsenmuuttujaksi, jotta "satunnaisuus" toimisi useaa riviä arvottaessa.
+                                // Mikäli arvoRivi-metodi loisi aina uuden Random-olion, monta peräkkäistä oliota 
+                                // saisi saman kellonajasta otetun seedin ja tuottaisi siksi saman lottorivin 
+
+        public Lotto(int tyyppi)
+        {
+            if (tyyppi < 1 || tyyppi > 4) tyyppi = 1;
+            this.tyyppi = tyyppi;
+            arpoja = new Random();
+
+            if (tyyppi == 1) // Lotto
+            {
+                pieninNumero = 1;
+                suurinNumero = 39;
+                lkmNumero = 7;
+            }
+            else if (tyyppi == 2) // Viking Lotto
+            {
+                pieninNumero = 1;
+                suurinNumero = 48;
+                lkmNumero = 6;
+            }
+            else if (tyyppi == 3) // Eurojackpot päänumerot
+            {
+                pieninNumero = 1;
+                suurinNumero = 50;
+                lkmNumero = 5;
+            }
+            else if (tyyppi == 4) // Eurojackpot tähtinumerot
+            {
+                pieninNumero = 1;
+                suurinNumero = 10;
+                lkmNumero = 2;
+            }
+        }
+
+        public List<int> arvoRivi()
+        {
+            // Luo numerot, joista arvotaan
+            List<int> numerot = new List<int>();
+            for (int i = pieninNumero; i <= suurinNumero; i++)
+            {
+                numerot.Add(i);
+            }
+
+            // Arpoo numerot
+            List<int> rivi = new List<int>();
+            
+            int numeroitaJaljella = suurinNumero;
+            int valinta;
+            for (int i = 0; i < lkmNumero; i++)
+            {
+                valinta = arpoja.Next(0, numeroitaJaljella);
+                rivi.Add(numerot[valinta]);
+                numerot.RemoveAt(valinta);
+                numeroitaJaljella--;
+            }
+
+            return rivi;
+        }
+    }
+}
--- Tehtava2Lotto/MainWindow.xaml.cs
+++ Tehtava2Lotto/MainWindow.xaml.cs
@@ -24,5 +24,78 @@ namespace Tehtava2Lotto
         {
             InitializeComponent();
         }
+
+        private void btnArvo_Click(object sender, RoutedEventArgs e)
+        {
+            int riveja;
+            string riviTeksti = "";
+            if (cbPeli.SelectedIndex >= 0 && int.TryParse(txtRiviLkm.Text, out riveja) && riveja > 0)
+            {
+                if (cbPeli.Text == "Eurojackpot")
+                {
+                    Lotto lottokone = new Lotto(3);
+                    Lotto lottokoneTahti = new Lotto(4);
+                    List<int> rivi, riviTahti;
+
+                    for (int j = 0; j < riveja; j++)
+                    {
+                        rivi = lottokone.arvoRivi();
+                        riviTahti = lottokoneTahti.arvoRivi();
+
+                        for (int i = 0; i < rivi.Count(); i++)
+                        {
+                            riviTeksti += rivi[i].ToString() + ", ";
+                        }
+
+                        for (int i = 0; i < riviTahti.Count(); i++)
+                        {
+                            riviTeksti += "*" + riviTahti[i].ToString();
+                            if (i < riviTahti.Count() - 1) riviTeksti += ", ";
+                        }
+
+                        riviTeksti += "\n";
+                    }
+                }
+                else if(cbPeli.Text == "Lotto" || cbPeli.Text == "Viking Lotto")
+                {
+                    Lotto lottokone;
+                    List<int> rivi;
+
+
+                    if (cbPeli.Text == "Lotto")
+                    {
+                        lottokone = new Lotto(1);
+                    }
+                    else
+                    {
+                        lottokone = new Lotto(2);
+                    }
+
+                    for (int j = 0; j < riveja; j++) {
+                        rivi = lottokone.arvoRivi();
+
+                        for (int i = 0; i < rivi.Count(); i++)
+                        {
+                            riviTeksti += rivi[i].ToString();
+                            if(i < rivi.Count() - 1) riviTeksti += ", ";
+                        }
+
+                        riviTeksti += "\n";
+                    }
+                }
+                else riviTeksti = "Virhe!";
+            }
+            else riviTeksti = "Virhe!";
+
+            txtRivit.Text = riviTeksti;
+            windowIkkuna.Title = "This is not your lucky day!";
+        }
+
+        private void btnTyhjenna_Click(object sender, RoutedEventArgs e)
+        {
+            cbPeli.SelectedIndex = 0;
+            txtRiviLkm.Text = "1";
+            txtRivit.Text = "";
+        }
     }
 }
