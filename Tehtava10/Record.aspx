e9bc793a4cb75bc133e91fef60fcbf87458016fe
Mikko Holopainen <mitaholo@gmail.com>
2015-12-09 15:23:27 +0200

    Tehtävä 10 palautus

---
+++ Tehtava10/Record.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Record.aspx.cs" Inherits="Record" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="recordForm" runat="server">
+    <div runat="server" id="MyServerControlDiv">
+        <asp:Label ID="lblError" runat="server" Text="" Visible="false"></asp:Label>
+        <asp:Image ID="imgCover" runat="server" height="200" width="200" Visible="false"/>
+        <h2><asp:Label ID="lblHeader" runat="server" Text=""/></h2>
+        <p><asp:Label ID="lblIsbn" runat="server" Text=""/></p>
+        <p><asp:Label ID="lblPrice" runat="server" Text=""/></p>
+        <asp:Repeater ID="rptSongs" runat="server">
+            <HeaderTemplate>
+                <p>Levyn biisit:</p>
+                <ul>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <li><%# (Container.DataItem as XElement).Attribute("name").Value %></li>
+            </ItemTemplate>
+            <FooterTemplate>
+                </ul>
+            </FooterTemplate>
+        </asp:Repeater>
+        <p><a href="Default.aspx">Takaisin</a></p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Tehtava10/Record.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+public partial class Record : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!String.IsNullOrEmpty(Request.QueryString["isbn"]))
+        {
+            String rootPath = Server.MapPath("~");
+            XDocument xml;
+            IEnumerable<XElement> records;
+
+            if (File.Exists(rootPath + "/LevykauppaX.xml"))
+            {
+                xml = XDocument.Load(rootPath + "/LevykauppaX.xml");
+
+                records = from record in xml.Root.Descendants("record")
+                          where record.Attribute("ISBN").Value == Request.QueryString["isbn"]
+                          select record;
+
+                if (records.Count<XElement>() == 1)
+                {
+                    XElement record = records.First<XElement>();
+
+                    if (File.Exists(rootPath + "Images\\" + record.Attribute("ISBN").Value + ".jpg"))
+                    {
+                        imgCover.Visible = true;
+                        imgCover.ImageUrl = "Images/" + record.Attribute("ISBN").Value + ".jpg";
+                    }
+
+                    lblHeader.Text = record.Attribute("Artist").Value + ": " + record.Attribute("Title").Value;
+                    lblIsbn.Text = "ISBN: " + record.Attribute("ISBN").Value;
+                    lblPrice.Text = "Hinta: " + record.Attribute("Price").Value + " €";
+
+                    IEnumerable<XElement> songs = record.Descendants("song");
+                    if (songs.Count<XElement>() > 0)
+                    {
+                        rptSongs.DataSource = record.Descendants("song");
+                        rptSongs.DataBind();
+                    }
+                }
+                else ShowError("Levyä ei löytynyt!");
+            }
+            else ShowError("Tietokantavirhe!");
+        }
+        else ShowError("Käyttäjän moka!");
+    }
+
+    private void ShowError(string text)
+    {
+        lblError.Visible = true;
+        lblError.Text = text;
+    }
+
+    protected void RecordBound(object sender, RepeaterItemEventArgs args)
+    {
+        Repeater repeaterRecord = sender as Repeater;
+        IEnumerable<XElement> enumerableRecord = repeaterRecord.DataSource as IEnumerable<XElement>;
+        XElement elementRecord = enumerableRecord.First<XElement>();
+
+        if (args.Item.ItemType == ListItemType.Item || args.Item.ItemType == ListItemType.AlternatingItem)
+        {
+            Repeater childRepeater = (Repeater)args.Item.FindControl("ChildRepeater");
+            childRepeater.DataSource = elementRecord.Descendants("song");
+            childRepeater.DataBind();
+        }
+    }
+}
